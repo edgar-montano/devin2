1930854d5fa502416528ceca374ec424593c1120
Edgar Montano <edgar.montano@nyu.edu>
2019-05-09 17:55:53 -0400

    implemented put  profile experienceto profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -154,4 +154,61 @@ router.delete("/", auth, async (req, res) => {
   }
 });
 
+//@route    PUT api/profile/experience
+//@desc     add profile experience
+//@access   Private
+
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "Title is required")
+        .not()
+        .isEmpty(),
+      check("company", "Company is required")
+        .not()
+        .isEmpty(),
+      check("from", "From date is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExperience = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      //rather then push the experience, we want to push it to top so we use unshift
+      profile.experience.unshift(newExperience);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 module.exports = router;
