af1f0b56c80f6c8957785ef2afdb4f1082a94a9c
Edgar Montano <edgar.montano@nyu.edu>
2019-05-09 18:44:01 -0400

    delete profile experience implemented

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -157,7 +157,7 @@ router.delete("/", auth, async (req, res) => {
 //@route    PUT api/profile/experience
 //@desc     add profile experience
 //@access   Private
-
+//@TODO     fix so you can also update previous experience
 router.put(
   "/experience",
   [
@@ -211,4 +211,25 @@ router.put(
     }
   }
 );
+
+//@route    DELETE api/profile/experience/:exp_id
+//@desc     delete profile experience
+//@access   Private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    // get remove index of experience
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
