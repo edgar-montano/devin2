78f73e92ac75b1c10aac35a5932639ee38ac5f15
Edgar Montano <edgar.montano@nyu.edu>
2019-05-09 17:17:17 -0400

    delete user and profile implemented

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -137,4 +137,21 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+//@route    DELETE api/profile
+//@desc     Delete profile, user, and posts
+//@access   Private
+//@TODO - remove users posts as well
+router.delete("/", auth, async (req, res) => {
+  try {
+    //remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    //remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: "User account has been deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
