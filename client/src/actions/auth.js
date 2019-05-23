86dc05f8ccef919a787e29ed5d94f8bbde5df066
Edgar Montano <em2532@nyu.edu>
2019-05-23 14:46:43 -0400

    fixed token data being retained from previous login

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -7,7 +7,8 @@ import {
   AUTH_ERROR,
   LOGIN_FAIL,
   LOGIN_SUCCESS,
-  LOGOUT
+  LOGOUT,
+  CLEAR_PROFILE
 } from "./types";
 
 import Axios from "axios";
@@ -78,5 +79,6 @@ export const login = (email, password) => async dispatch => {
 // Logout and clear profile
 
 export const logout = () => dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+import Spinner from "../layout/Spinner";
+import { Link } from "react-router-dom";
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading }
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
-  return <div>Dashboard</div>;
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user">Welcome {user && user.name}</i>
+      </p>
+      {profile !== null ? (
+        <Fragment>ProfileAvailable</Fragment>
+      ) : (
+        <Fragment>
+          <p>You do not have a profile created.</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create profile
+          </Link>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Dashboard.propTypes = {
