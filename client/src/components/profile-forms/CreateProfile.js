3cf886fe23ec7f2ad458ee6c14998b323e6de4cc
Edgar Montano <em2532@nyu.edu>
2019-05-24 11:21:18 -0400

    composed profile forms

---
+++ client/src/components/profile-forms/CreateProfile.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+const CreateProfile = props => {
+  const [formData, setFormData] = useState({
+    company: "",
+    website: "",
+    location: "",
+    status: "",
+    skills: "",
+    githubusername: "",
+    bio: "",
+    twitter: "",
+    facebook: "",
+    linkedin: "",
+    youtube: "",
+    instagram: ""
+  });
+
+  const {
+    company,
+    website,
+    location,
+    status,
+    skills,
+    githubusername,
+    bio,
+    twitter,
+    facebook,
+    linkedin,
+    youtube,
+    instagram
+  } = formData;
+
+  return <div />;
+};
+
+CreateProfile.propTypes = {};
+
+export default CreateProfile;
