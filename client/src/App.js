720524b4b3ca561631fc1f5f9d04f8c7ab220986
Edgar Montano <em2532@nyu.edu>
2019-05-27 15:18:13 -0400

    create profile frontend implemented

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
+import CreateProfile from "./components/profile-forms/CreateProfile";
 //redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -33,6 +34,11 @@ const App = () => {
 
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
