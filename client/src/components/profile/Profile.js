1bfd8f14a9323c5d18af0eb764f0958827841a13
Edgar Montano <em2532@nyu.edu>
2019-05-29 17:28:15 -0400

    added profile education section

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience.js";
+import ProfileEducation from "./ProfileEducation";
 const Profile = ({
   getProfileById,
   profile: { profile, loading },
@@ -45,6 +46,21 @@ const Profile = ({
                     <ProfileExperience
                       key={experience._id}
                       experience={experience}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No experience credentials</h4>
+              )}
+            </div>
+            <div className="profile-edu bg-white p-2">
+              <h2 className="text-primar">Education</h2>
+              {profile.education.length > 0 ? (
+                <Fragment>
+                  {profile.education.map(education => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
                     />
                   ))}
                 </Fragment>
