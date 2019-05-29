4630e01e0dc0b3f794100de5e74b44ea5490e635
Edgar Montano <em2532@nyu.edu>
2019-05-29 15:08:18 -0400

    added additional actions and reducers

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -5,7 +5,9 @@ import {
   PROFILE_ERROR,
   UPDATE_PROFILE,
   CLEAR_PROFILE,
-  ACCOUNT_DELETED
+  ACCOUNT_DELETED,
+  GET_PROFILES,
+  GET_REPOS
 } from "./types";
 
 // get current users profile
@@ -13,6 +15,51 @@ export const getCurrentProfile = () => async dispatch => {
   try {
     const res = await axios.get("/api/profile/me");
     dispatch({ type: GET_PROFILE, payload: res.data });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// get all user profiles
+export const getProfiles = () => async dispatch => {
+  //prevents flashing of past user profile in some instances
+  dispatch({ type: CLEAR_PROFILE });
+
+  //get all other profiles
+  try {
+    const res = await axios.get("/api/profile");
+    dispatch({ type: GET_PROFILES, payload: res.data });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// get profile by id
+export const getProfileById = userId => async dispatch => {
+  //get all other profiles
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`);
+    dispatch({ type: GET_PROFILE, payload: res.data });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+// get github repos
+export const getGithubRepos = username => async dispatch => {
+  //get all other profiles
+  try {
+    const res = await axios.get(`/api/profile/github/${username}`);
+    dispatch({ type: GET_REPOS, payload: res.data });
   } catch (err) {
     dispatch({
       type: PROFILE_ERROR,
