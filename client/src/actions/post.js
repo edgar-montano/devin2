d398c78ea2d7d10954a8dcc9d9a51606946a3241
Edgar Montano <em2532@nyu.edu>
2019-05-30 15:03:56 -0400

    delete post action implemented

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR, PROFILE_ERROR, UPDATE_LIKES } from "./types";
+import {
+  GET_POSTS,
+  POST_ERROR,
+  PROFILE_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST
+} from "./types";
 
 //get posts
 export const getPosts = () => async dispatch => {
@@ -32,6 +38,20 @@ export const removeLike = postId => async dispatch => {
   try {
     const res = await axios.put(`/api/posts/unlike/${postId}`);
     dispatch({ type: UPDATE_LIKES, payload: { postId, likes: res.data } });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//delete post
+export const deletePost = postId => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/${postId}`);
+    dispatch({ type: DELETE_POST, payload: { postId } });
+    dispatch(setAlert("Post Removed", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
