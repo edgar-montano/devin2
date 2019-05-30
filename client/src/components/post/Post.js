165fa582714b8bc273ccd77496eef5c96c427f1a
Edgar Montano <em2532@nyu.edu>
2019-05-30 16:01:07 -0400

    add comment implemented

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,8 +3,10 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
+
 import { getPost } from "../../actions/post";
 import { Link } from "react-router-dom";
+import CommentForm from "./CommentForm";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -19,6 +21,7 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         Back to Posts
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
     </Fragment>
   );
 };
