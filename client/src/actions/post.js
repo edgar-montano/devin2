fedfd90ca93f27f2e14274f57d1bb017fb29d86a
Edgar Montano <em2532@nyu.edu>
2019-05-30 14:58:24 -0400

    liked and unlike functionality implemented

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -19,7 +19,7 @@ export const getPosts = () => async dispatch => {
 export const addLike = postId => async dispatch => {
   try {
     const res = await axios.put(`/api/posts/like/${postId}`);
-    dispatch({ type: UPDATE_LIKES, payload: { id, likes: res.data } });
+    dispatch({ type: UPDATE_LIKES, payload: { postId, likes: res.data } });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
@@ -31,7 +31,7 @@ export const addLike = postId => async dispatch => {
 export const removeLike = postId => async dispatch => {
   try {
     const res = await axios.put(`/api/posts/unlike/${postId}`);
-    dispatch({ type: UPDATE_LIKES, payload: { id, likes: res.data } });
+    dispatch({ type: UPDATE_LIKES, payload: { postId, likes: res.data } });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,8 +3,10 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
 import { connect } from "react-redux";
-
+import { addLike, removeLike } from "../../actions/post";
 const PostItem = ({
+  addLike,
+  removeLike,
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date }
 }) => {
@@ -21,12 +23,16 @@ const PostItem = ({
         <p class="post-date">
           Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
         </p>
-        <button type="button" class="btn btn-light">
+        <button onClick={e => addLike(_id)} type="button" class="btn btn-light">
           <i class="fas fa-thumbs-up" />
           {"  "}
           <span> {likes.length}</span>
         </button>
-        <button type="button" class="btn btn-light">
+        <button
+          onClick={e => removeLike(_id)}
+          type="button"
+          class="btn btn-light"
+        >
           <i class="fas fa-thumbs-down" />
         </button>
         <Link to={`/post/${_id}`} class="btn btn-primary">
@@ -53,5 +59,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  {}
+  { addLike, removeLike }
 )(PostItem);
