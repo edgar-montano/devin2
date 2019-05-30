655003aca9569c73704f119436ec97402a92231e
Edgar Montano <em2532@nyu.edu>
2019-05-30 15:15:22 -0400

    add post action created

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -5,7 +5,8 @@ import {
   POST_ERROR,
   PROFILE_ERROR,
   UPDATE_LIKES,
-  DELETE_POST
+  DELETE_POST,
+  ADD_POST
 } from "./types";
 
 //get posts
@@ -52,6 +53,25 @@ export const deletePost = postId => async dispatch => {
     const res = await axios.delete(`/api/posts/${postId}`);
     dispatch({ type: DELETE_POST, payload: { postId } });
     dispatch(setAlert("Post Removed", "success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+//add post
+export const addPost = formData => async dispatch => {
+  const config = {
+    header: {
+      "Content-Type": "application/json"
+    }
+  };
+
+  try {
+    const res = await axios.post("/api/posts", formData, config);
+    dispatch({ type: ADD_POST, payload: res.data });
+    dispatch(setAlert("Post Created", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
